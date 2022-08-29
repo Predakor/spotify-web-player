9153a49deb988939a8d91fdf10b26bec00f77847
Predakor <patrykbusko@gmail.com>
2022-08-29 11:44:28 +0200

    Added Styling

--- components/Layout/Nav.tsx
+++ components/Layout/Nav.tsx
@@ -1,23 +1,34 @@
-import LoginButton from '@components/Button/LoginButton';
 import UserAccount from '@components/UserAccount/UserAccount';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { ReactNode } from 'react';
+
+function NavLink({ href, children }: { href: string; children?: ReactNode }) {
+  const path = useRouter().pathname;
+  const isActivePage = path === href;
+  const activeClass = 'text-green-500';
+
+  return (
+    <Link href={href}>
+      <a
+        className={`${
+          isActivePage ? activeClass : ''
+        } hover:text-green-600 transition-colors `}
+      >
+        {children}
+      </a>
+    </Link>
+  );
+}
 
 function Nav() {
   return (
-    <nav className="flex gap-1">
-      <Link className="text-green-600" href={'/'}>
-        Home
-      </Link>
-      <Link className="text-green-600" href={'/playlist'}>
-        Playlist
-      </Link>
-      <Link className="text-green-600" href={'/stats'}>
-        Stats
-      </Link>
-      <Link href={'profile'}>
-        <UserAccount />
-      </Link>
-      <LoginButton />
+    <nav className="flex gap-1 p-1 text-xl">
+      <NavLink href={'/'}>Home</NavLink>
+      <NavLink href={'/playlist'}>Playlist</NavLink>
+      <NavLink href={'/stats'}>Stats</NavLink>
+
+      <UserAccount />
     </nav>
   );
 }
